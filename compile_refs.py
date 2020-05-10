ed34ad64c7f0f8bcb6dda2f8057ff1a953163587
barlette <leobarlette@gmail.com>
2020-05-10 11:49:15 -0300

    diss 10/05 1

--- compile_refs.py
+++ compile_refs.py
@@ -7,7 +7,7 @@ commands = [
     ['bibtex', sys.argv[1] + '.aux'],
     ['pdflatex', sys.argv[1] + '.tex'],
     ['pdflatex', sys.argv[1] + '.tex'],
-    ['mv', sys.argv[1] + '.pdf', '/mnt/d/Dropbox/dissertation/']
+    ['cp', sys.argv[1] + '.pdf', '/mnt/d/Dropbox/dissertation/']
 ]
 
 for c in commands:
