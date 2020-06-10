f015a1fd55421470a9ba64c59e585d80ebd8106f
barlette <leobarlette@gmail.com>
2020-06-10 04:06:53 -0300

    mods-10-06-2020 correntes

--- compile_refs.py
+++ compile_refs.py
@@ -2,13 +2,27 @@
 
 import subprocess, sys
 
-commands = [
-    ['pdflatex', sys.argv[1] + '.tex'],
-    ['bibtex', sys.argv[1] + '.aux'],
-    ['pdflatex', sys.argv[1] + '.tex'],
-    ['pdflatex', sys.argv[1] + '.tex'],
-    ['cp', sys.argv[1] + '.pdf', '/mnt/d/Dropbox/dissertation/']
-]
+#print(sys.argv[1])
+#print(sys.argv[2])
+
+if sys.argv[1] == '-d':
+	#print('aaa')
+	commands = [
+    		['pdflatex', '-interaction=nonstopmode', sys.argv[2]],
+    		['bibtex', sys.argv[2]],
+    		['pdflatex', '-interaction=nonstopmode', sys.argv[2]],
+    		['pdflatex', '-interaction=nonstopmode', sys.argv[2]],
+    		['cp', sys.argv[2] + '.pdf', '/mnt/d/Dropbox/dissertation/']
+	]
+else:
+	commands = [
+    		['pdflatex', sys.argv[1] + '.tex'],
+    		['bibtex', sys.argv[1] + '.aux'],
+    		['pdflatex', sys.argv[1] + '.tex'],
+    		['pdflatex', sys.argv[1] + '.tex'],
+    		['cp', sys.argv[1] + '.pdf', '/mnt/d/Dropbox/dissertation/']
+	]
 
 for c in commands:
-    subprocess.call(c)
+	print(c)
+	subprocess.call(c)
